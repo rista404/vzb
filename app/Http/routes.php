ca5d8269c47da1e432a347222e6b0838c5eb5f38
Ivan Jovanovic <ivanjov96@gmail.com>
2015-12-05 14:39:09 +0100

    Group admin routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,24 +15,25 @@ Route::get('/', function () {
     return view('index');
 });
 
-// Authentication routes...
-Route::get('admin/login', 'Auth\AuthController@getLogin');
-Route::post('admin/login', 'Auth\AuthController@postLogin');
-Route::get('admin/logout', 'Auth\AuthController@getLogout');
-Route::get('admin', 'AdminController@index');
-Route::get('admin/schools', 'AdminController@getSchools');
-Route::get('admin/school/{id}', 'AdminController@getSchool');
-Route::post('admin/school/{id}', 'AdminController@editSchool');
-Route::get('admin/delete/photo/{id}', 'AdminController@deletePhoto');
-Route::get('admin/dorms', 'AdminController@getDorms');
-Route::get('admin/dorm/add', 'AdminController@addDorm');
-Route::post('admin/dorm/add', 'AdminController@saveDorm');
-Route::get('admin/dorm/{id}', 'AdminController@getDorm');
-Route::post('admin/dorm/{id}', 'AdminController@editDorm');
-Route::get('admin/dorm/delete/{id}', 'AdminController@deleteDorm');
+// Admin routes
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('login', 'Auth\AuthController@getLogin');
+    Route::post('login', 'Auth\AuthController@postLogin');
+    Route::get('logout', 'Auth\AuthController@getLogout');
+    Route::get('admin', 'AdminController@index');
+    Route::get('schools', 'AdminController@getSchools');
+    Route::get('school/{id}', 'AdminController@getSchool');
+    Route::post('school/{id}', 'AdminController@editSchool');
+    Route::get('delete/photo/{id}', 'AdminController@deletePhoto');
+    Route::get('dorms', 'AdminController@getDorms');
+    Route::get('dorm/add', 'AdminController@addDorm');
+    Route::post('dorm/add', 'AdminController@saveDorm');
+    Route::get('dorm/{id}', 'AdminController@getDorm');
+    Route::post('dorm/{id}', 'AdminController@editDorm');
+    Route::get('dorm/delete/{id}', 'AdminController@deleteDorm');
+});
 
-
-//API v1
+//API v1 routes
 Route::group(['prefix' => 'api/v1'], function() {
     Route::any('schools', 'ApiController@getSchools');
     Route::any('schools/{id}', 'ApiController@getSchool');
