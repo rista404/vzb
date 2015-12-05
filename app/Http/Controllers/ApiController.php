c5db39671ceaa24b7d4031ef3f4432cdb0ca7f8d
Ivan Jovanovic <ivanjov96@gmail.com>
2015-12-05 09:43:08 +0100

    TakeAll static function

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -3,51 +3,19 @@
 use App\School;
 use GuzzleHttp\Client;
 use Illuminate\Http\Request;
+use Cache;
 
 class ApiController extends Controller {
 
     public function getSchools() {
-        $client = new Client;
-
-        $res = $client->request("GET", "http://opendata.mpn.gov.rs/1337/get.php?dataset=vsustanove2016&lang=en&term=json");
-
-        $schools = json_decode($res->getBody());
-
-        $schools_from_db = School::where('id', 153)->with('photos')->get();
-
-        foreach ($schools as $key=>$school){
-            foreach ($schools_from_db as $key_db=>$school_from_db){
-                if(!$this->isInBelgrade($school->univerzitet, $school->nazivu)) {
-                    unset($schools[$key]);
-                    continue;
-                }
-
-                if($school->id == $school_from_db->id) {
-                    $schools[$key]->bus = $school_from_db->bus;
-
-                    $photos = [];
-                    foreach ($school_from_db->photos as $photo) {
-                        $photos[]['location'] = $photo->location;
-                    }
-                    $schools[$key]->photos = $photos;
-                }
-            }
-        }
+        $schools = School::takeAll();
 
         return $schools;
     }
 
     public function getSchool($id) {
-        $school = School::find($id);
+        $school = School::takeOne($id);
 
         return $school;
-    }
-
-    private function isInBelgrade($univerzitet, $naziv = "") {
-        if(strpos($univerzitet,'Београд') !== false || ($univerzitet == "" && strpos($naziv,'Београд') !== false)) {
-            return true;
-        }
-
-        return false;
     }
 }
--- app/School.php
+++ app/School.php
@@ -3,6 +3,8 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use GuzzleHttp\Client;
+use Cache;
 
 class School extends Model {
 
@@ -15,5 +17,60 @@ class School extends Model {
 
     public function photos() {
         return $this->hasMany('App\Photo', 'school_id', 'id');
+    }
+
+    public static function takeAll() {
+        if (Cache::has('schools')) {
+            $schools = Cache::get('schools');
+        }
+        else {
+            $schools = self::storeSchoolsInCache();
+        }
+
+        return $schools;
+    }
+
+    private static function storeSchoolsInCache() {
+        $client = new Client;
+
+        $res = $client->request("GET", "http://opendata.mpn.gov.rs/1337/get.php?dataset=vsustanove2016&lang=en&term=json");
+
+        $schools = json_decode($res->getBody());
+
+        $schools_from_db = self::where('id', 153)->with('photos')->get();
+
+        foreach ($schools as $key=>$school){
+            foreach ($schools_from_db as $key_db=>$school_from_db){
+                if(!self::isInBelgrade($school->univerzitet, $school->nazivu)) {
+                    unset($schools[$key]);
+                    continue;
+                }
+
+                if($school->id == $school_from_db->id) {
+                    $schools[$key]->bus = $school_from_db->bus;
+
+                    $photos = [];
+                    foreach ($school_from_db->photos as $photo) {
+                        $photos[]['location'] = $photo->location;
+                    }
+                    $schools[$key]->photos = $photos;
+                }
+            }
+        }
+
+        //Store in cache for 7 days
+        $cached_schools = Cache::remember('schools', 10080, function() use ($schools) {
+            return $schools;
+        });
+
+        return $cached_schools;
+    }
+
+    private static function isInBelgrade($univerzitet, $naziv = "") {
+        if(strpos($univerzitet,'Београд') !== false || ($univerzitet == "" && strpos($naziv,'Београд') !== false)) {
+            return true;
+        }
+
+        return false;
     }
 }
