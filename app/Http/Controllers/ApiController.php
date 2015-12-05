58125bc8dae78dbbe22a50b2ca2a0f6272ed22b7
Ivan Jovanovic <ivanjov96@gmail.com>
2015-12-05 08:42:52 +0100

    Fetching schools from API

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -1,12 +1,31 @@
 <?php namespace App\Http\Controllers;
 
 use App\School;
+use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 
 class ApiController extends Controller {
 
     public function getSchools() {
-        $schools = School::all();
+        $client = new Client;
+
+        $res = $client->request("GET", "http://opendata.mpn.gov.rs/1337/get.php?dataset=vsustanove2016&lang=en&term=json");
+
+        $schools = json_decode($res->getBody());
+
+        $schools_from_db = School::all();
+
+        foreach ($schools as $key=>$school){
+            foreach ($schools_from_db as $key_db=>$school_from_db){
+                if($school->univerzitet != "Универзитет у Београду" && $school->univerzitet != "Универзитет уметности у Београду" && ($school->univerzitet == "" && strpos($school->nazivu,'Београд') === false)) {
+                    unset($schools[$key]);
+                    continue;
+                }
+
+                if($school->id == $school_from_db->id)
+                    $schools[$key]->bus = $school_from_db->bus;
+            }
+        }
 
         return $schools;
     }
